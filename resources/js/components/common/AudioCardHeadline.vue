5cd0cc9318d430974c6936f48e5f71db3cdab586
ihamzehald <ihamzehald@gmail.com>
2021-02-02 00:09:52 +0400

    tune page

---
+++ resources/js/components/common/AudioCardHeadline.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "AudioCardHeadline"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/pages/Tune.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="col-12">
+<!--    <Header></Header>-->
+    <div class="row mg-10-pr">
+        <div class="col-10 offset-1">
+            <VideoConvertor></VideoConvertor>
+        </div>
+    </div>
+
+    <AudioCardGrid class="mg-10-pr"></AudioCardGrid>
+
+</div>
+</template>
+
+<script>
+import VideoConvertor from "../common/VideoConvertor";
+import AudioCardGrid from "../common/AudioCardGrid";
+import Header from "../layout/Header";
+import Vue from "vue";
+
+export default {
+    name: "Home",
+    components:{
+        VideoConvertor,
+        AudioCardGrid,
+        Header
+    },
+    mounted() {
+        let payload = {
+            "searchType" : "videos"
+        }
+
+        this.$root.$emit('triggerSetSearchType',payload)
+
+        let activeMenuPayload = {
+            "activeMenu" : "home"
+        }
+
+        this.$root.$emit('triggerSetActiveMenu',activeMenuPayload)
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
